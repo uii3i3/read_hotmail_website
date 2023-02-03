1941fb50e610963b339a80d2e2131e04d4911a58
k3lu0 <demo@gmail.com>
2023-02-03 14:30:56 +0000

    upload some

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
+function toggleLoading(disable=true){
+    loader = document.getElementById("loader")
+    if (disable){
+        loader.classList.add('d-none')
+    }else{
+        loader.classList.remove('d-none')
+    }
+}
+function addMessage(messages){
+    messageElm  = document.getElementById('message')
+    if (messageElm.classList.contains('d-none'))
+        messageElm.classList.remove('d-none')
+    for (var message of messages){
+        h1 = document.createElement('h1')
+        h1.innerText = message.From
+        messageElm.appendChild(h1)
+        // document.getElementById('message')
+        // doc
+    }
+}
+function getDataSuccess(data, account){
+    console.log(data)
+    hotmailUseElm = document.querySelector('#hotmail-use')
+    hotmailUseElm.innerText = account
+    if (data.status === 200){
+        hotmailUseElm.classList.add('alert-success')
+        addMessage(data.message)
+    }else{
+        hotmailUseElm.classList.add('alert-primary')
+    }
+}
 function getAllMail(account){
-    user = account.split("|")[0]
-    pwd = account.split("|")[1]
+    console.log('run check account:', account)
+    toggleLoading(false)
+    var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+    };
+
+    fetch("https://utopian-delightful-pruner.glitch.me/get-message/" + account, requestOptions)
+    .then(response => response.json())
+    .then(result => getDataSuccess(result,account))
+    .catch(error => console.log('error', error))
+    .finally(()=>toggleLoading(toggleLoading(true)))
 }
 async function test(){
     // let url = 'http://139.180.152.4/get-acc';
@@ -8,7 +49,9 @@ async function test(){
 //   .then((response) => response.json())
 //   .then((data) => console.log(data));
 
-    let url = 'https://catfact.ninja/fact';
+    // let url = 'https://catfact.ninja/fact';
+    // let url = 'https://65.20.71.249:5002/';
+    let url = 'https://ninth-abyssinian-sunstone.glitch.me/';
     let res = await fetch(url);
     let data = await res.json()
     console.log(data)
@@ -23,8 +66,10 @@ document.addEventListener("DOMContentLoaded", () => {
         if (event.target.tagName === 'LI'){
             hotmailListDiv.querySelector('.active').classList.remove('active')
             event.target.classList.add('active')
-            document.querySelector('#hotmail-use').innerText = event.target.innerText
+            account = event.target.innerText
+            // disable content element
 
+            getAllMail(account)
         }
     })
     // click functions
